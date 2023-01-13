baaef3874c665fb8a8ef2e4591dde64e8ec94a36
jmhr0 <jherram0603@g.educaand.es>
2023-01-13 14:57:57 +0100

    Clase 13/01 Introduccion a los arrays

---
+++ CalculadoraBasica/src/org/ieslosremedios/daw1/prog/ut4/Introduccion_arrays.java
@@ -0,0 +1,14 @@
+package org.ieslosremedios.daw1.prog.ut4;
+
+public class Introduccion_arrays {
+    public static void main(String[] args) {
+        Integer [] array ={1,2,3,4,5};
+        System.out.println(array);
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+
+}
